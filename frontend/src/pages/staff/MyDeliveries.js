bfc30780d41cb08ada569a57f9f6c05f5df9d734
Prashantkk1001 <prashantkhedkar2006@gmail.com>
2026-04-25 14:08:45 +0530

    update the codes

--- frontend/src/pages/staff/MyDeliveries.js
+++ frontend/src/pages/staff/MyDeliveries.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { deliveryAPI, ordersAPI } from '../../services/api';
